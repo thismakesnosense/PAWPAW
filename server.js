144677968e3c3140b543e7f630f51b9a121fac0d
nathan olenik <nolenik23@outlook.com>
2020-03-21 18:38:23 -0400

    mongodbURI

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-
+const connectionURI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -15,7 +15,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+
+mongoose.connect( connectionURI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
